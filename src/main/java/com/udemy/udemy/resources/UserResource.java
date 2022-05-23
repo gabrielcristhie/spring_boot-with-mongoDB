ad4640c841a5df8549a826ce74ff610490ccd5a9
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2022-05-22 23:25:49 -0300

    Retornando os posts de um usuario

--- src/main/java/com/udemy/udemy/resources/UserResource.java
+++ src/main/java/com/udemy/udemy/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.udemy.udemy.domain.Post;
 import com.udemy.udemy.domain.User;
 import com.udemy.udemy.dto.UserDTO;
 import com.udemy.udemy.services.UserService;
@@ -54,5 +55,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build(); 
+	}
+
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
