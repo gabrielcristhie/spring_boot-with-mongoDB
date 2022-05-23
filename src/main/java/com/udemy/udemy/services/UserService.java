ba29de69ee45ff6cf8b4e6ad71289c4030e1c4db
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2022-05-22 21:42:13 -0300

    Inserção de usuários com Post

--- src/main/java/com/udemy/udemy/services/UserService.java
+++ src/main/java/com/udemy/udemy/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.udemy.udemy.domain.User;
+import com.udemy.udemy.dto.UserDTO;
 import com.udemy.udemy.repository.UserRepository;
 import com.udemy.udemy.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,12 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
